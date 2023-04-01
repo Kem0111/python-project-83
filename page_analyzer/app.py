59edda10ea3335f3143091dd6cd9cfbd072b5b88
Камран <kem@192.168.1.13>
2023-04-01 16:52:10 +0300

    update app.py

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,7 +1,12 @@
 from flask import Flask
+from dotenv import load_dotenv
+import os
 
+load_dotenv()
 
 app = Flask(__name__)
+app.secret_key = os.getenv("SECRET_KEY")
+api_key = os.getenv("API_KEY")
 
 
 @app.route('/')
